fe32af58de3a8c10dd3958dac2ef76df2371adbb
Johnn <johnndavid27@gmail.com>
2024-07-03 23:38:45 -0300

    Project_construct

---
+++ construct/roles.py
@@ -0,0 +1,16 @@
+from rolepermissions.roles import AbstractUserRole
+
+
+class Gerente(AbstractUserRole):
+    available_permissions = {
+        'cadastrar_produtos': True,
+        'liberar_desconto': True,
+        'cadastrar_vendedor': True,
+        
+    }
+
+class Vendedor(AbstractUserRole):
+    available_permissions = {
+        'realizar_vendas': True
+        
+    }
